444b6eca78b92ec4ea8c52e719db98b9385260aa
Emma Gerigscott <elgerig@gmail.com>
2023-06-27 21:38:41 -0700

    create react form

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -7,10 +7,16 @@ class TicketControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      formVisibleOnPage: false
+      formVisibleOnPage: false,
+      mainTicketList: []
     };
   }
 
+  handleAddingsNewTicketToList = (newTicket) => {
+    const newMainTicketList = this.state.mainTicketList.concat(newTicket);
+    this.setState({mainTicketList: newMainTicketList,
+    formVisibleOnPage: false});
+  }
   handleClick = () => {
     this.setState(prevState => ({
       formVisibleOnPage: !prevState.formVisibleOnPage
@@ -21,10 +27,10 @@ class TicketControl extends React.Component {
     let currentlyVisibleState = null;
     let buttonText = null;
     if (this.state.formVisibleOnPage) {
-      currentlyVisibleState = <NewTicketForm />;
+      currentlyVisibleState = <NewTicketForm onNewTicketCreation={this.handleAddingsNewTicketToList} />;
       buttonText = "Return to Ticket List";
     } else {
-      currentlyVisibleState = <TicketList />
+      currentlyVisibleState = <TicketList ticketList={this.state.mainTicketList}/>
       buttonText = "Add Ticket";
     }
     return (
